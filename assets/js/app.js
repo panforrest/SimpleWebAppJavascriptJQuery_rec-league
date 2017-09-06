d7b6165340d4b019dcce1ddedaf74ad7337ea555
panforrest <guoqianp@gmail.com>
2017-09-06 06:20:39 -0400

    render current user list

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,23 @@
 (function(){
 
   console.log('HELLO REC LEAGUE!')
+  var turbo = Turbo({site_id: '59afc034bca7290011590d56'})
+
+  turbo.fetch('user', null, function(err, data){
+  	if (err) {
+  	  return
+  	}
+
+  	console.log('CURRENT USERS: '+JSON.stringify(data))
+  	var users = data.results
+
+  	var list = ''
+  	users.forEach(function(user, i){
+  	  list += '<li>'+user.name+'</li>'
+  	})
+
+  	$('#current-user-list').html(list)
+  })
 
   $('#btn-add-user').click(function(event){
   	event.preventDefault()
@@ -12,7 +29,7 @@
 
   	console.log('ADD USER: '+JSON.stringify(user))
     
-    var turbo = Turbo({site_id: '59afc034bca7290011590d56'})
+    
     turbo.createUser(user, function(err, data){
       if(err){
       	alert('Error: '+err.message)
