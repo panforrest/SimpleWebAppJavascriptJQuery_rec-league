c4ebcae55a79422dc03f2e8b00a977753589e4df
panforrest <guoqianp@gmail.com>
2017-09-06 06:09:04 -0400

    create first user

--- assets/js/app.js
+++ assets/js/app.js
@@ -11,6 +11,17 @@
     }
 
   	console.log('ADD USER: '+JSON.stringify(user))
+    
+    var turbo = Turbo({site_id: '59afc034bca7290011590d56'})
+    turbo.createUser(user, function(err, data){
+      if(err){
+      	alert('Error: '+err.message)
+      	return
+      }
+
+      console.log('USER CREATED: '+JSON.stringify(data))
+    })
+
   })
 
 })()
