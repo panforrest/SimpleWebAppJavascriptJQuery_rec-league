22acfdecfafcf64b549ebf4552199d655c1fd7a6
panforrest <guoqianp@gmail.com>
2017-09-06 21:02:24 -0400

    team name is accessed from url

--- assets/js/team.js
+++ assets/js/team.js
@@ -1,5 +1,13 @@
 (function(){
 
-  console.log('HELLO! TEAM')
+  var url = window.location.href
+  var parts = url.split('#')
+
+  if (parts.length == 1) { //no team specified
+  	return
+  }
+
+  var teamName = parts[1]
+  console.log('HELLO! TEAM Page: '+teamName) //console.log('HELLO! TEAM Page: '+window.location.href)
 
 })()
